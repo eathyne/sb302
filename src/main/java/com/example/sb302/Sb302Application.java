082bdcb936f1f1028d7582d183eca1667e3fcace
Eathyn Edwards <eathynedwards@Earyns-MacBook-Air.local>
2019-11-25 16:04:41 -0500

    Initial commit

---
+++ src/main/java/com/example/sb302/Sb302Application.java
@@ -0,0 +1,13 @@
+package com.example.sb302;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb302Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb302Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sb302/Sb302ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb302;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb302ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
